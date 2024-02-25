ce1aef8938fc1e1657f235842d92f521fbdb2616
eman62 <emanhagag62@gmail.com>
2024-02-25 11:20:27 +0200

    change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Footer from './Components/footer';
 
 function App() {
   return (
     <div className="App">
        local crafts
+       <Footer></Footer>
     </div>
   );
 }
--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,1 +1,8 @@
+import Box from "@mui/material/Box"
+const Footer=()=>{
+  return (
+    <Box></Box>
+  )
+}
 
+export default Footer;
